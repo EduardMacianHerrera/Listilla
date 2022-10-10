e67bef33542de53ea4bed90a0313939fbcd7e07d
EduardMacianHerrera <emacianherrera.cf@iesesteveterradas.cat>
2022-10-10 16:46:29 +0200

    Imagenes añadidas y randomizadas. Botón de ordenar y ordenación implementada

--- app/src/main/java/com/example/listilla/MainActivity.java
+++ app/src/main/java/com/example/listilla/MainActivity.java
@@ -8,18 +8,29 @@ import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     // Model: Record (intents=puntuació, nom)
-    class Record {
+    class Record implements Comparator<Record> {
         public int intents;
         public String nom;
+
+        Record(){
+
+        }
 
         public Record(int _intents, String _nom ) {
             intents = _intents;
             nom = _nom;
+        }
+
+        @Override
+        public int compare(Record record, Record t1) {
+            return record.intents - t1.intents;
         }
     }
     // Model = Taula de records: utilitzem ArrayList
@@ -52,6 +63,9 @@ public class MainActivity extends AppCompatActivity {
                     convertView = getLayoutInflater().inflate(R.layout.list_item, container, false);
                 }
                 // "Pintem" valors (també quan es refresca)
+                int[] imagenes = {R.drawable.bobobolechuga,R.drawable.bobobozanahoria,R.drawable.bobobo,R.drawable.donpatch,R.drawable.donpatch2};
+                Random random = new Random();
+                ((ImageView) convertView.findViewById(R.id.imageView)).setBackgroundResource(imagenes[random.nextInt(imagenes.length)]);
                 ((TextView) convertView.findViewById(R.id.nom)).setText(getItem(pos).nom);
                 ((TextView) convertView.findViewById(R.id.intents)).setText(Integer.toString(getItem(pos).intents));
                 return convertView;
@@ -76,6 +90,15 @@ public class MainActivity extends AppCompatActivity {
                     records.add(new Record(random.nextInt(101),nombres[random.nextInt(nombres.length)]));
                 }
                 // notificar l'adapter dels canvis al model
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        Button ordena = (Button) findViewById(R.id.ordenaButton);
+        ordena.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Collections.sort(records,new Record());
                 adapter.notifyDataSetChanged();
             }
         });
